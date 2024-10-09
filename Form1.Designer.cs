0afd29fd0d3b649e5be335a9992d83bae8226066
MetiBluer <133645933+MetiBluer@users.noreply.github.com>
2024-10-09 15:03:24 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace DB_ACT1_046_Tolentino_Francesz
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grdResult = new System.Windows.Forms.DataGridView();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.txtKeyword = new System.Windows.Forms.TextBox();
+            this.cboBrand = new System.Windows.Forms.ComboBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grdResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdResult
+            // 
+            this.grdResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdResult.Location = new System.Drawing.Point(34, 38);
+            this.grdResult.Name = "grdResult";
+            this.grdResult.Size = new System.Drawing.Size(240, 306);
+            this.grdResult.TabIndex = 0;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(450, 189);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // txtKeyword
+            // 
+            this.txtKeyword.Location = new System.Drawing.Point(311, 105);
+            this.txtKeyword.Name = "txtKeyword";
+            this.txtKeyword.Size = new System.Drawing.Size(100, 20);
+            this.txtKeyword.TabIndex = 2;
+            this.txtKeyword.TextChanged += new System.EventHandler(this.txtKeyword_TextChanged);
+            // 
+            // cboBrand
+            // 
+            this.cboBrand.FormattingEnabled = true;
+            this.cboBrand.Items.AddRange(new object[] {
+            "Acura",
+            "Aston Martin",
+            "Audi",
+            "Bentley",
+            "BMW",
+            "Buick",
+            "Cadillac",
+            "Chevrolet",
+            "Chrysler",
+            "Dodge",
+            "Eagle",
+            "Ford",
+            "Geo",
+            "GMC",
+            "Honda",
+            "Hummer",
+            "Hyundai",
+            "Infiniti",
+            "Isuzu",
+            "Jeep",
+            "Kia",
+            "Land Rover",
+            "Lexus",
+            "Lincoln",
+            "Lotus",
+            "Maserati",
+            "Mazda",
+            "Mercedes-Benz",
+            "Mercury",
+            "Mitsubishi",
+            "Nissan",
+            "Oldsmobile",
+            "Plymouth",
+            "Pontiac",
+            "Porsche",
+            "Ram",
+            "Saab",
+            "Saturn",
+            "Scion",
+            "Shelby",
+            "Spyker",
+            "Subaru",
+            "Suzuki",
+            "Toyota",
+            "Volkswagen",
+            "Volvo"});
+            this.cboBrand.Location = new System.Drawing.Point(427, 104);
+            this.cboBrand.Name = "cboBrand";
+            this.cboBrand.Size = new System.Drawing.Size(132, 21);
+            this.cboBrand.TabIndex = 3;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(311, 189);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.cboBrand);
+            this.Controls.Add(this.txtKeyword);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.grdResult);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdResult;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox txtKeyword;
+        private System.Windows.Forms.ComboBox cboBrand;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace DB_ACT1_046_Tolentino_Francesz
+{
+    public partial class Main : Form
+    {
+        string connStr = "server=localhost; database=db_act1; uid=root; pwd=uslt; port=3306";
+        MySqlConnection conn;
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            conn = new MySqlConnection(connStr);
+            string query = "select * from car where model= '" + txtKeyword.Text + "' and brand '" + cboBrand.Text + "' ";
+            conn.Open();
+            MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn);
+            adapter.Fill(dt);
+            conn.Close();
+            grdResult.DataSource = dt;
+        }
+
+        private void txtKeyword_TextChanged(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            conn = new MySqlConnection(connStr);
+            string query = "select * from car where model= '" + txtKeyword.Text + "' and brand '" + cboBrand.Text + "' ";
+            conn.Open();
+            MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn);
+            adapter.Fill(dt);
+            conn.Close();
+            grdResult.DataSource = dt;
+        }
+    }
+}
